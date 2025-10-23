c8ad07c9222ab31b2be01ee5d0acd3621ec00825
Raviteja Kandagatla <ravitejakandagatla1998@gmail.com>
2025-10-23 08:04:32 +0530

    feat: add new app.js file

---
+++ app.js
@@ -0,0 +1,7 @@
+function handleCreatePost(){
+            //console.log("CLicked on create post button")//
+            document.getElementById("post-form-container").style.display = "block"
+        }   //handleCreatePost()//
+        function hidePostForm() {
+            document.getElementById("post-form-container").style.display = "none"
+        }
